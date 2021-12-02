634e8dee74b1d261d2ebec0c12185a9853694ee1
NoMamesWey1 <92000349+NoMamesWey1@users.noreply.github.com>
2021-12-01 22:52:23 -0800

    Update Fire.java

--- src/Fire.java
+++ src/Fire.java
@@ -6,13 +6,12 @@ public class Fire extends Pokemon{
     @Override
     public String getAttackMenu(int atkType) {
         if(atkType == 1){
-            String basicMenu = super.getAttackMenu(1);
-            return basicMenu;
-        } else if(atkType == 2){
+            return super.getAttackMenu(atkType);
+        }else{
             String specialMenu = "1. Ember\n2. Fire Blast\n3. Fire Punch";
             return specialMenu;
         }
-        return null;
+        
     }
 
     //TODO: remove the comment when the getNumAttackMenuItems is implemented.
@@ -25,15 +24,7 @@ public class Fire extends Pokemon{
     //@Override
     public String getAttackString(int atkType, int move) {
         if(atkType == 1){
-            if(move == 1){
-                return "SLAMMED";
-            }else if (move == 2){
-                return "TACKLED";
-            }else if (move == 3){
-                return "PUNCHED";
-            }else{
-                return "OFFED";
-            }
+            return super.getAttackString(atkType,move);
         }else if (atkType == 2){
             if(move == 1){
                 return "GOT ENCAPSULATED";
@@ -61,22 +52,10 @@ public class Fire extends Pokemon{
     //TODO: remove the slashes when the getAttackDamage is implemented.
     //@Override
     public int getAttackDamage(int atkType, int move){
+        int specialDamage = 0;
         if(atkType == 1){
-            int basicDamage = 0;
-            if(move == 1){
-                basicDamage = (int)(Math.random() * 5);
-                return basicDamage;
-            }else if (move == 2){
-                basicDamage = (int)(Math.random() * 3) + 2;
-                return basicDamage;
-            }else if (move == 3){
-                basicDamage = (int)(Math.random() * 4) + 1;
-                return basicDamage;
-            }else{
-                return basicDamage;
-            }
+            return super.getAttackDamage(atkType,move);
         }else if (atkType == 2){
-            int specialDamage = 0;
             if(move == 1){
                 specialDamage = (int)(Math.random()*3)+0;
                 return specialDamage;
@@ -101,7 +80,7 @@ public class Fire extends Pokemon{
      * */
     //TODO: remove the comment when the getNumAttackMenuItems is implemented.
     //@Override
-    public int getAttackMultiplier(Pokemon p, int atkType){
+    public double getAttackMultiplier(Pokemon p, int atkType){
         int damage = (int) (Math.random() * 3) + 1;
         if(getType() == 0){
             double amplified = battleTable[0][0];
