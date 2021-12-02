0c6a29309c4cdc224b8308095cd25ae07129da3e
NoMamesWey1 <92000349+NoMamesWey1@users.noreply.github.com>
2021-12-02 14:40:41 -0800

    Update HpDown.java

--- src/HpDown.java
+++ src/HpDown.java
@@ -1,5 +1,8 @@
 public class HpDown extends PokemonDecorator{
-
+/**
+  Description: lowering down the hp 
+  @param Pokemon p - is the pokemon we are lowering down the hp
+  */
   public HpDown(Pokemon p){
     super(p," -HP",-(int)(Math.random()*2)+1);;
   }
