dd1102e7a266e511024ec902ac5053b1ec00fc55
NoMamesWey1 <92000349+NoMamesWey1@users.noreply.github.com>
2021-12-02 14:41:13 -0800

    Update HpUp.java

--- src/HpUp.java
+++ src/HpUp.java
@@ -1,4 +1,8 @@
 public class HpUp extends PokemonDecorator{
+  /**
+  Description: raising the hp 
+  @param Pokemon p - is the pokemon we are raising the hp
+  */
   public HpUp(Pokemon p){
     super(p," HP",(int)(Math.random()*2)+1);
   }
