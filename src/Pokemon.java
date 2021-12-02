e0f0f638b75b444cb7168751030c167c71174bdf
NoMamesWey1 <92000349+NoMamesWey1@users.noreply.github.com>
2021-12-02 15:01:29 -0800

    Update Pokemon.java

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -4,20 +4,35 @@ import java.lang.Math;
  */
 public abstract class Pokemon extends Entity{
   public static final double[][] battleTable = {{1,.5,2},{2,1,.5},{.5,2,1}};
-
+/**
+  Description: the constructor for the pokemon
+  @param n - is the name
+  @param h - is the hp
+  @param m - is the max hp
+  */
   public Pokemon(String n, int h, int m){
     super(n,h,m);
   }
-
+/**
+  Description: getting the attack menu
+  @return - returning the menu
+  */
   public String getAttackTypeMenu(){
     String menu = "1. Basic Attacks\n2. Special Attacks";
     return menu;
   }
-
+/**
+  Description: getting the amount of options it contains
+  @return - returning the amount of options
+  */
   public int getNumAttackTypeMenuItems(){
     return 2;
   }
-
+/**
+  Description: getting the basic Attacks
+  @param atkType - is which atkType they chose
+  @return - the basic menu attacks
+  */
   public String getAttackMenu(int atkType){
     if(atkType == 1){
       String basicMenu = "1. Slam\n2. Tackle\n3. Punch";
@@ -26,11 +41,21 @@ public abstract class Pokemon extends Entity{
       return null;
     }
   }
-
+/**
+  Description: getting the amount of options it contains
+  @param atkType - the atkType they chose
+  @return - returning the amount of options
+  */
   public int getNumAttackMenuItems(int atkType){
     return 3;
   }
-
+/**
+  Description: attacking the pokemon
+  @param p - the pokemon
+  @param atkType - which atkType they user chose
+  @param move - which move the user chose
+  return - returning the attack string
+  */
   public String attack(Pokemon p, int atkType, int move){
     int damage = (int) (getAttackDamage(atkType, move) * getAttackMultiplier(p,atkType)) + getAttackBonus(atkType);
     p.takeDamage(damage);
@@ -38,7 +63,12 @@ public abstract class Pokemon extends Entity{
     String attackString = p.getName() + " " + getAttackString(atkType,move) + " and takes " + damage + " damage.";
     return attackString;
   }
-
+/**
+  Description: getting the string of the attack
+  @param atkType - which atkType it is
+  @param move - which move they are doing
+  return -  the string of the attack
+  */
   public String getAttackString(int atkType, int move){
     if(move == 1){
       return "is SLAMMED";
@@ -50,7 +80,12 @@ public abstract class Pokemon extends Entity{
       return "is PUNCHED";
     }
   }
-
+/**
+  Description: getting the damage
+  @param atkType - which attack it is
+  @param move - which move they are doing
+  @return - returning the damage
+  */
   public int getAttackDamage(int atkType, int move){
     if(move == 1){
       int damage = (int)(Math.random()*5);
@@ -63,15 +98,27 @@ public abstract class Pokemon extends Entity{
       return damage;
     }
   }
-
+/**
+Description: getting the attack multiplyed 
+@param p - which pokemon it is
+@param atkType - which attack it is
+@return - returning the multipler
+*/
   public double getAttackMultiplier(Pokemon p, int atkType){
       return 1;
   }
-
+/**
+Description: getting the bonus amount 
+@param atkType - which attack it is
+@return - the bonus amount
+*/
   public int getAttackBonus(int atkType){
     return 0;
   }
-
+/**
+Description: getting the type of the pokemon
+@return - the type it is
+*/
   public int getType(){
     if(this instanceof Fire){
       return 0;
